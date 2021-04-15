d708dc94b8ff1cb950a1f5de336f7f7230cb7ee5
ubay <58719280+alice-node@users.noreply.github.com>
2021-04-15 22:45:16 +0700

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-cd /home
-bash -c 'echo "killall screen
-rm /home/idena-*
-cd /home
-curl -s https://api.github.com/repos/idena-network/idena-go/releases/latest | grep browser_download_url | grep idena-node-linux-0.* | cut -d '"' -f 4 | wget -qi -
-mv idena-node-linux* idena-go
-chmod +x idena-go
-screen -d -m ./idena-go
-echo Update sukses Bos" > update.sh'
-chmod +x update.sh
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install unzip curl screen -y
+
+mkdir datadir && cd datadir
+mkdir idenachain.db && cd idenachain.db
+#wget "https://idena.site/idenachain.db.zip"
+wget "https://sync.idena.site/idenachain.db.zip"
+unzip idenachain.db.zip && rm idenachain.db.zip
+cd ..
+cd ..
+
+curl -s https://api.github.com/repos/idena-network/idena-go/releases/latest \
+| grep browser_download_url \
+| grep idena-node-linux-0.* \
+| cut -d '"' -f 4 \
+| wget -qi -
+mv idena-* idena-go && chmod +x idena-go
